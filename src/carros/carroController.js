b3bd9f271284be0ed45d783a591e8e5e7cb84a7a
andre.rogerio <andre.rogerio@192.168.201.18>
2014-09-16 17:04:30 -0300

    Ajustes no Projeto

--- src/carros/carroController.js
+++ src/carros/carroController.js
@@ -1,9 +1,9 @@
 /**
  * Created by andre.rogerio on 11/09/2014.
  */
-app.controller('carrosController', ['$scope', 'carrosAPI', '$location', '$anchorScroll', 'fabricanteAPI', function ($scope, carrosAPI, $location, $anchorScroll, fabricanteAPI) {
+app.controller('carrosController', ['$scope', 'carrosAPI', '$location', '$anchorScroll', 'fabricanteAPI', function($scope, carrosAPI, $location, $anchorScroll, fabricanteAPI) {
 
-        $scope.carroTitulo = "Carros Já";
+        $scope.carroTitulo = "Carros JÃ¡";
         $scope.carro = {};
         $scope.carro.fabricante = {};
         $scope.listaCarros = [];
@@ -15,7 +15,7 @@ app.controller('carrosController', ['$scope', 'carrosAPI', '$location', '$anchor
         $scope.templatePesquisaCarro = {name: 'pesquisaCarro', url: 'carros/pesquisaCarros.html'};
         $scope.carroShow = {};
 
-        $scope.adicionarCarro = function (carro) {
+        $scope.adicionarCarro = function(carro) {
             if ($scope.editIndex === -1) {
                 $scope.listaCarros.push(angular.copy(carro));
             }
@@ -28,64 +28,76 @@ app.controller('carrosController', ['$scope', 'carrosAPI', '$location', '$anchor
             $scope.$$childTail.carroForm.$setPristine();
         };
 
-        $scope.carregarAnos = function () {
+        $scope.carregarAnos = function() {
             for (i = 1900; i < 2015; i++) {
                 $scope.anos.push(i);
             }
         };
 
-        $scope.excluirCarro = function (carro) {
+        $scope.excluirCarro = function(carro) {
             $scope.listaCarros.splice($scope.listaCarros.indexOf(carro), 1);
             delete $scope.carro;
             $scope.carro = {};
             $scope.editIndex = -1;
         };
 
-        $scope.editarCarro = function (carro) {
+        $scope.editarCarro = function(carro) {
             $scope.carro = angular.copy(carro);
+            
+            var fabricante;
+            for (fabricante in $scope.fabricantes) {
+                if ($scope.fabricantes[fabricante].nome === carro.fabricante.nome)
+                {
+                    $scope.carro.fabricante = $scope.fabricantes[fabricante];
+                    console.log($scope.fabricantes[fabricante]);
+                }
+            }
+            
+            
+            
             $scope.editIndex = $scope.listaCarros.indexOf(carro);
             $scope.gotoCadastro();
         };
 
-        $scope.showCarro = function (carro) {
+        $scope.showCarro = function(carro) {
             $scope.carroShow = angular.copy(carro);
         };
 
-        $scope.cleanShow = function (carro) {
+        $scope.cleanShow = function(carro) {
             $scope.carroShow = {};
         };
 
 
-        $scope.ordenarCarrosPor = function (campo) {
+        $scope.ordenarCarrosPor = function(campo) {
             $scope.campoOrderBy = campo;
             console.log($scope.campoOrderBy);
             $scope.reverse = !$scope.reverse;
             console.log($scope.reverse);
         };
 
-        $scope.gotoCadastro = function () {
+        $scope.gotoCadastro = function() {
             $location.hash('cadastro');
             $anchorScroll();
         };
 
-        $scope.gotoTopo = function () {
+        $scope.gotoTopo = function() {
             $location.hash('topo');
             $anchorScroll();
         };
-        
-        $scope.iniciarListas = function ()
+
+        $scope.iniciarListas = function()
         {
             $scope.carregarAnos();
-            
-            carrosAPI.carregarCarros().success(function (data, status) {
+
+            carrosAPI.carregarCarros().success(function(data, status) {
                 $scope.listaCarros = data;
-            }).error(function (data, status) {
+            }).error(function(data, status) {
                 console.error(data);
             });
-            
-            fabricanteAPI.carregarFabricantes().success(function (data, status) {
+
+            fabricanteAPI.carregarFabricantes().success(function(data, status) {
                 $scope.fabricantes = data;
-            }).error(function (data, status) {
+            }).error(function(data, status) {
                 console.error(data);
             });
         };
