729914424a04446134c853ada6ab6e1d6e549900
andre.rogerio <andre.rogerio@192.168.201.18>
2014-09-26 17:40:52 -0300

    Criação de Exemplos

--- src/carros/route.js
+++
@@ -1,5 +0,0 @@
-app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when("/home", {templateUrl:"home.html"});
-    $routeProvider.when("/carros", {templateUrl:"carros/carros.html", controller:"carrosController"});
-    $routeProvider.otherwise({redirectTo: "/home"});
-}]);
